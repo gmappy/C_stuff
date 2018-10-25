fff54316ef0b28f1cef154b4180e64841a7e58b6
Elephant <34973210+gmappy@users.noreply.github.com>
2018-10-26 00:36:55 +0200

    Re-did the entire code on the article
    made the code much more effiecient

--- prototype.c
+++ prototype.c
@@ -1,93 +1,42 @@
-/**Super-mini calculator for and by gmappy... with love thanks ;) */
 #include <stdio.h>
-#include <string.h>
+#include <unistd.h>
 
-int	main()
+int main()
 {
-	int loose = 0;
-	char choice;
-	
-/** Addition */
-	int addition;
-	int addition1;
-	int sum_addition;
-	
-/** Multiplication */
-	int times;
-	int times1;
-	int sum_times;
-	
-/** Division */	
-	int divide;
-	int divide1;
-	float sum_divide;
-	
-/** Subtraction */
-	int minus;
-	int minus1;
-	int sum_minus;
-	
-/** Make-shift Log-In */
-	char name[15];
-	
-	printf("\tPlease enter username below:\n");
-	scanf("%s",name);
-	printf("\tWelcome:%s\n",name);
-/** End of Make-shift Log-In */	
-	
-	printf("Please choose desired arithmetic symbol\n");
-	printf("\ta - Addition\n");
-	printf("\tb - Mutliplication\n");
-	printf("\tc - Division\n");
-	printf("\td - Subtraction\n");
-	choice = getc(stdin);
-	getc(stdin);
-	
-/** I'm pretty sure you get how this works, pretty simple really */
+	char name[20];
 
-	if(choice == 1)
-	{
-		printf("Please enter two intergers\n");
-		scanf("%d",addition);
-		scanf("%d",addition1);
-		
-		sum_addition = addition + addition1;
-		
-		printf("%d",sum_addition);
-	}
-}
-	
+	int i;
+	int d1,d2;
+	int total;
 
-	else if(choice == 'b')
-	{
-		printf("Please enter two integers\n");
-		scanf("%d",times);
-		scanf("%d",times1);
-		
-		sum_times = times * times1;
-		
-		printf("%d",sum_times);
-	}
-	else if(choice == 'c')
-	{
-		printf("Please enter two intergers\n");
-		scanf("%d",divide);
-		scanf("%d",divide1);
-		
-		sum_divide = divide / divide1;
-		
-		printf("%f",sum_divide);
-	}
-	else if(choice == 'd')
-	{
-		printf("Please enter two intergers\n");
-		scanf("%d",minus);
-		scanf("%d",minus1);
-		
-		sum_minus = minus - minus1;
-		
-		printf("%d",sum_minus);
-	}
 
+	printf("\tPlease enter username\n");
+	scanf("\t%s",&name);
+	printf("\t Welcome %s, Please Choose desired arithmetic sign below\n");
+
+	printf("\t1 - Addition\n");
+	printf("\t2 - Subtraction\n");
+	scanf("\t%d",&i);
+
+	if (i == 1)
+	{
+		printf("\tPlease enter first digit\n");
+		scanf("\t%d",&d1);
+		printf("\tPlease enter second digit\n");
+		scanf("\t%d",&d2);
+
+		total = (d1 + d2);
+
+		printf("\tResult = %d",total);
+	}
+	else if (i == 2)
+	{
+		 printf("\tPlease enter first digit\n");
+ 24                 scanf("\t%d",&d1);
+ 25                 printf("\tPlease enter second digit\n");
+ 26                 scanf("\t%d",&d2);
+ 27
+ 28                 total = (d1 - d2);
+	}
 	return 0;
-}	
+}
